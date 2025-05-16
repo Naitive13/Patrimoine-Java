3ca937997e266919ded3df76a8ef738a227e6d44
Naitive13 <hei.titan.2@gmail.com>
2025-05-16 12:19:47 +0300

    chore: resolve merge conflict

--- src/main/java/com/titan/model/TrainDeVie.java
+++ src/main/java/com/titan/model/TrainDeVie.java
@@ -1,18 +1,43 @@
 package com.titan.model;
 
+import lombok.Getter;
+
+import java.time.Duration;
 import java.time.LocalDate;
+import java.time.Month;
 
+import static com.titan.model.Argent.ariary;
+import static java.time.temporal.ChronoUnit.MONTHS;
+
+@Getter
 public final class TrainDeVie extends Possession {
-    public TrainDeVie(String nom, Argent valeur, LocalDate aDateDe) {
-        super(nom, valeur, aDateDe);
+  private final Compte financeur;
+  private final int jourDePonction;
+  private final LocalDate debutDePonction;
+
+  public TrainDeVie(
+      String nom,
+      Argent valeur,
+      LocalDate aDateDe,
+      Compte financeur,
+      int jourDePonction,
+      LocalDate debutDePonction) {
+    super(nom, valeur, aDateDe);
+    this.financeur = financeur;
+    this.jourDePonction = jourDePonction;
+    this.debutDePonction = debutDePonction;
+  }
+
+  @Override
+  public TrainDeVie projectionFuture(LocalDate dateFuture) {
+    if (dateFuture.isBefore(this.getADateDe())) {
+      return new TrainDeVie(nom, ariary(0d), aDateDe, financeur, jourDePonction, debutDePonction);
     }
 
-    @Override
-  public Possession projectionFuture(LocalDate dateFuture) {
-    if (dateFuture.isBefore(this.getADateDe())) {
-      Argent argent = new Argent(0d, this.getValeur().getDevise());
-      return new Compte(this.getNom(), argent, this.getADateDe());
+    int moisEcoulee = (int) MONTHS.between(dateFuture,debutDePonction);
+
+    if(dateFuture.getDayOfMonth() >= jourDePonction){
+      Argent valeurFuture = financeur.getValeur().soustraire(ariary(valeur.getMontant()*moisEcoulee));
     }
-    return this;
   }
 }
