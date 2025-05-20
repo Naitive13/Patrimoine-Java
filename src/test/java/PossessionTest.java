8d3649ebdf55d53da6fc90123bc239a5530bfa00
Naitive13 <hei.titan.2@gmail.com>
2025-05-20 11:55:56 +0300

    test: add unit test for Compte::projectionFutur

--- src/test/java/PossessionTest.java
+++ src/test/java/PossessionTest.java
@@ -32,8 +32,8 @@ public class PossessionTest {
   }
 
   @Test
-  void projection_future_pour_un_train_de_vie() {
-    var compteCourant = new Compte("compte courant", ariary(600_000), LocalDate.of(2024, 5, 13));
+  void projection_future_pour_un_train_de_vie_et_un_compte() {
+    var compteCourant = new Compte("compte courant", ariary(600_000), LocalDate.of(2024, 5, 13),LocalDate.of(2024, 5, 13));
     var trainDeVie =
         new TrainDeVie(
             "vie quotidienne",
@@ -43,9 +43,11 @@ public class PossessionTest {
             1,
             LocalDate.of(2024, 5, 13));
 
-    TrainDeVie actuel = trainDeVie.projectionFuture(LocalDate.of(2024, 7, 26));
+    TrainDeVie trainDeVieActuel = trainDeVie.projectionFuture(LocalDate.of(2024, 7, 26));
+    Compte compteCourantActuel = compteCourant.projectionFuture(LocalDate.of(2024, 7, 26));
 
     assertEquals(
-        1_000_000, actuel.getValeur().getMontant(), "projection du 26 juin 2024");
+        1_000_000, trainDeVieActuel.getValeur().getMontant(), "projection du train de vie pour le 26 juin 2024");
+    assertEquals(-400_000, compteCourantActuel.getValeur().getMontant(), "projection du compte courant pour le 26 juin 2024");
   }
 }
