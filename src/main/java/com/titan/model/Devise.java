135a293a4589f8a62ef042069302523ac308237c
Naitive13 <hei.titan.2@gmail.com>
2025-05-21 11:03:39 +0300

    feat: add valeurEnAriary to Devise

--- src/main/java/com/titan/model/Devise.java
+++ src/main/java/com/titan/model/Devise.java
@@ -1,7 +1,16 @@
 package com.titan.model;
 
+import lombok.Getter;
+
+@Getter
 public enum Devise {
-  ARIARY,
-  USD,
-  EURO
+  ARIARY(1d),
+  USD(4200d),
+  EURO(4000d);
+
+  private final double valeurEnAriary;
+
+  Devise(double tauxDeChangeVersAriary) {
+    this.valeurEnAriary = tauxDeChangeVersAriary;
+  }
 }
