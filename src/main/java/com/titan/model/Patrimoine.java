b1de30eb3d64ea8d1b9e46e25956645f14b664f7
Naitive13 <hei.titan.2@gmail.com>
2025-05-21 11:04:59 +0300

    feat: implement Patrimoine::projectionFuture

--- src/main/java/com/titan/model/Patrimoine.java
+++ src/main/java/com/titan/model/Patrimoine.java
@@ -2,14 +2,16 @@ package com.titan.model;
 
 import java.time.LocalDate;
 import java.util.Set;
+import java.util.stream.Collectors;
 
-public record Patrimoine(
-    Personne possesseur, LocalDate date, Double valeur, Set<Possession> possessions) {
+public record Patrimoine(Personne possesseur, LocalDate date, Set<Possession> possessions) {
 
-  public Double projectionFuture(LocalDate dateFuture) {
-    return possessions.stream()
-        .map(possession -> possession.projectionFuture(dateFuture).getValeur().getMontant())
-        .reduce(Double::sum)
-        .get();
+  public Patrimoine projectionFuture(LocalDate dateFuture) {
+    Set<Possession> possessionsFutur =
+        possessions.stream()
+            .map(possession -> possession.projectionFuture(dateFuture))
+            .collect(Collectors.toSet());
+
+    return new Patrimoine(possesseur, dateFuture, possessionsFutur);
   }
 }
